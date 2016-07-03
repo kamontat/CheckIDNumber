059e2e8e0808b4a4d2d7a71b14f1f9910e655269
kamontat <kamontat_c@hotmail.com>
2016-07-04 00:16:38 +0700

    can enter id number and update in TextFile

--- src/com/kamontat/gui/EnterNumberPage.java
+++ src/com/kamontat/gui/EnterNumberPage.java
@@ -1,30 +1,69 @@
 package com.kamontat.gui;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 public class EnterNumberPage extends JDialog {
 	private JPanel contentPane;
-	private JButton buttonOK;
-	private JButton buttonCancel;
+	private JButton okBtn;
+	private JButton cancelBtn;
 	private JTextField textField;
+	private JLabel totalNumberLabel;
 
 	public EnterNumberPage() {
 		setContentPane(contentPane);
 		setModal(true);
 		pack();
 
-		buttonOK.addActionListener(e -> onOK());
+		textField.getDocument().addDocumentListener(new DocumentListener() {
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				warn();
+			}
 
-		buttonCancel.addActionListener(e -> onCancel());
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				warn();
+			}
+
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				warn();
+			}
+		});
+
+		okBtn.addActionListener(e -> onOK());
+
+		cancelBtn.addActionListener(e -> onCancel());
 
 		// call onCancel() on ESCAPE
 		contentPane.registerKeyboardAction(e -> onCancel(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 	}
 
 	private void onOK() {
-		System.out.println(textField.getText());
+		// get dir
+		File dir = Paths.get("").toAbsolutePath().toFile();
+		if (dir.isDirectory()) {
+			File textFile = new File(dir.getPath() + "/output.txt");
+			try {
+				FileWriter writer = new FileWriter(textFile, true);
+
+				textFile.createNewFile();
+				writer.write(textField.getText() + "\n");
+
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Done");
 	}
 
 	private void onCancel() {
@@ -32,7 +71,29 @@ public class EnterNumberPage extends JDialog {
 		dispose();
 	}
 
-	private void run(Point point) {
+	private void warn() {
+		if (isAllNumberIn(textField.getText())) {
+			// have only Integer or Double. :)
+			okBtn.setEnabled(true);
+		} else {
+			// have some String too. :(
+			okBtn.setEnabled(false);
+		}
+	}
+
+	private Boolean isAllNumberIn(String input) {
+		// update label
+		totalNumberLabel.setText("Enter ID Number (" + input.length() + ")");
+		// check every char in input String
+		for (int i = 0; i < input.length(); i++) {
+			char aChar = input.charAt(i);
+
+			if (!Character.isDigit(aChar)) return false;
+		}
+		return input.length() == 13;
+	}
+
+	public void run(Point point) {
 		setMinimumSize(new Dimension(0, 0));
 		setLocation(point);
 		setVisible(true);
