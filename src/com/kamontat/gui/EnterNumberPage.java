4cab025863b12901ac4dcf8d33b01937295684c0
kamontat <kamontat_c@hotmail.com>
2016-07-09 19:54:15 +0700

    update duplicate real time

--- src/com/kamontat/gui/EnterNumberPage.java
+++ src/com/kamontat/gui/EnterNumberPage.java
@@ -51,24 +51,17 @@ public class EnterNumberPage extends JDialog {
 
 	private void onOK() {
 		try {
-			if (isDuplicate(textField.getText(), idList)) {
-				messageLabel.setText("Error (Duplicate ID)");
-				messageLabel.setForeground(new Color(255, 0, 0));
-			} else {
+			if (!isDuplicate(textField.getText(), idList)) {
 				FileWriter writer = new FileWriter(textFile, true);
 				writer.write(textField.getText() + "\n");
 				writer.close();
-
-				messageLabel.setText("Correct");
-				messageLabel.setForeground(new Color(0, 255, 0));
-
+				setMessage("Correct ID (Saved)", new Color(0, 255, 0));
 				// rewrite new list in program whe program update
 				assignIDList();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 		pack();
 	}
 
@@ -79,25 +72,38 @@ public class EnterNumberPage extends JDialog {
 
 	private void warn() {
 		if (isAllNumberIn(textField.getText())) {
-			// have only Integer or Double. :)
-			okBtn.setEnabled(true);
+			if (textField.getText().length() == 13) {
+				if (!isDuplicate(textField.getText(), idList)) {
+					// have only Integer or Double. :)
+					okBtn.setEnabled(true);
+					setMessage("OK (Good ID)", new Color(0, 200, 0));
+				} else {
+					// id mustn't duplicate with the old one
+					okBtn.setEnabled(false);
+					setMessage("Error (Duplicate ID)", new Color(255, 0, 0));
+				}
+			} else {
+				// id must have 13 character
+				okBtn.setEnabled(false);
+				setMessage("Warning (not equal 13)", new Color(255, 170, 0));
+			}
 		} else {
 			// have some String too. :(
 			okBtn.setEnabled(false);
+			setMessage("Error (Have Alphabet)", new Color(255, 0, 0));
 		}
 	}
 
 	private boolean isAllNumberIn(String input) {
 		// update label
 		totalNumberLabel.setText("Enter ID Number (" + input.length() + ")");
-
 		// check every char in input String
 		for (int i = 0; i < input.length(); i++) {
 			char aChar = input.charAt(i);
 
 			if (!Character.isDigit(aChar)) return false;
 		}
-		return input.length() == 13;
+		return true;
 	}
 
 	private boolean isDuplicate(String text, ArrayList<String> list) {
@@ -105,6 +111,11 @@ public class EnterNumberPage extends JDialog {
 			if (aString.equals(text)) return true;
 		}
 		return false;
+	}
+
+	private void setMessage(String message, Color color) {
+		messageLabel.setText(message);
+		messageLabel.setForeground(color);
 	}
 
 	public void run(Point point) {
