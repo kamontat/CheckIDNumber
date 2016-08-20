075422c1e4a76fbd7455bc9afd14c6a62ca0a70e
kamontat <kamontat_c@hotmail.com>
2016-08-21 00:23:08 +0700

    add province data at first term 11 provinces

---
+++ src/com/kamontat/code/file/Provinces.java
@@ -0,0 +1,75 @@
+package com.kamontat.code.file;
+
+import com.kamontat.code.object.IDNumber;
+
+import java.io.*;
+import java.util.*;
+import java.util.function.Consumer;
+
+import static com.kamontat.code.window.Display.dir;
+
+/**
+ * @author kamontat
+ * @since 20/8/59 - 23:15
+ */
+public class Provinces {
+	private static String[][] allData;
+
+	public static void read() {
+		ArrayList<String[]> allData = new ArrayList<>();
+
+		Consumer<String> consumer = s -> {
+			String[] array = s.split(" ");
+			allData.add(array);
+		};
+
+		try {
+			File file = new File(dir.getPath() + "/src/resources/province/allProvinces.txt");
+			BufferedReader br = new BufferedReader(new FileReader(file));
+
+			br.lines().forEach(consumer); // add data into arrays
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		Provinces.allData = allData.toArray(new String[allData.size()][]);
+	}
+
+	/**
+	 * @param id
+	 * 		only number digit `2-5`
+	 * @return At element 0 is The Province <br>
+	 * At element 1 is The District
+	 */
+	public static String[] getProvince(IDNumber id) {
+		String province = "";
+		String[] info = new String[2];
+		info[0] = "";
+		info[1] = "";
+
+		for (String[] data : allData) {
+			for (int j = 0; j < allData[0].length; j++) {
+				// Province
+				if (data[0].length() != 4) {
+					// clear new province
+					province = "";
+					for (int i = 1; i < data.length; i++) {
+						province += data[i] + " ";
+					}
+				}
+
+				// District
+				if (data[0].length() == 4) {
+					if (id.getIDAddress().equals(data[0])) {
+						info[0] = province;
+						for (int i = 1; i < data.length; i++) {
+							info[1] += data[i] + " ";
+						}
+						return info;
+					}
+				}
+			}
+		}
+		return new String[]{"Unknown Province", "Unknown District"};
+	}
+}
