7a6df7b72314a9a1208785435c57b4242bc5ee80
kamontat <kamontat_c@hotmail.com>
2016-10-20 00:35:31 +0700

    expend todolist

--- src/com/kamontat/main/Main.java
+++ src/com/kamontat/main/Main.java
@@ -12,9 +12,3 @@ public class Main {
 		LauncherPage page = new LauncherPage();
 	}
 }
-
-// TODO: 10/19/2016 AD Reopen program button, or refresh loading file
-// TODO: 10/19/2016 AD Status of text-file if it isn't ex
-// TODO: 10/19/2016 AD New error about first digit can't be `9`
-// TODO: 10/19/2016 AD information page must add create_time
-// TODO: 10/19/2016 AD fixed program crash when search many id in show page
